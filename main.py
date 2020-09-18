3652cbcc6a85a396abc38476f388e19d083cd8ec
bigbreadguy <schadenlim@gmail.com>
2020-09-19 00:14:22 +0900

    Update main.py
    Add argparser

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import glob
 import random
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-from generator import LimGenerator
+from generator import *
 
 cwd = os.getcwd()
 png_list = list(glob.glob(os.path.join(cwd, "layers","*.png")))
@@ -16,5 +17,26 @@ for png in png_list:
   img = Image.open(png)
   img_list.append(img)
 
+class opts():
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="lim_generator",
+                                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        self.parser.add_argument("-t", "--type", default="display",
+                                 choices=["display", "collage"], type=str, dest="type")
+        self.parser.add_argument("-c", "--change", default=False,
+                                 choices=[True, False], type=bool, dest="change")
+    
+    def parse(self, args : str = None):
+        if args == None:
+            opt = self.parser.parse_args()
+        else:
+            opt = self.parser.parse_args(args)
+
+        return opt
+
+args = opts().parse()
 gen = LimGenerator(img_list)
-gen.collage()
+if args.type == "display":
+    gen.display(args.change)
+else:
+    gen.collage()
